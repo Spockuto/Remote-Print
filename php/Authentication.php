24ed2d23fcf6d4219c0a33586a550fbbd5c7170d
Manix <vigneshmanix@gmail.com>
2015-09-09 15:27:13 +0530

    ldap auth

--- php/Authentication.php
+++ php/Authentication.php
@@ -9,15 +9,18 @@ $_SESSION["username"] = $username;
 $_SESSION["password"] = $password;
 
 
+$host = '10.0.0.39';
+$domain = 'octa.edu';
+$ad = ldap_connect("ldap://{$host}") or die('Could not connect to LDAP server.');
+ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3);
+ldap_set_option($ad, LDAP_OPT_REFERRALS, 0);
 
-if($username == "106114104" && $password == "Octa104"){
-	header("Location: ../html/print.php");
-	exit();
-}
-
-else{
-	header("Location: ../html/wrong.html");
-	exit();
+$result = @ldap_bind($ad, $username.'@'.$domain,$password);
+ldap_unbind($ad);
+if ($result == TRUE) {
+    header("Location: ../html/print.php");
+} else {
+    header("Location: ../html/wrong.html");
 }
 
 ?>
